1a4875cba2d2e6cb1b8896db668e065f37072982
Michael <michaeldam.dev@gmail.com>
2022-10-14 13:38:47 -0600

    proto type

--- idle_game.js
+++ idle_game.js
@@ -1,1 +1,29 @@
-let points = 0;
+
+
+let points = document.getElementById("points");
+let cookie = document.getElementById("cookie");
+let price = document.getElementById("cost");
+let upgrade = document.getElementsByClassName("upgrade");
+
+let clickPower = 1;
+let score = 0;
+let cost = 1;
+
+
+cookie.addEventListener("click", ()=>{
+    score = clickPower + score;
+    points.innerHTML=score;
+
+});
+
+upgrade[0].addEventListener("click", ()=>{
+        if( cost <= score){
+            score = score - cost;
+            cost = cost + Math.floor(cost*1.1);
+            clickPower ++;
+            points.innerHTML=score;
+            price.innerHTML=cost;
+        }
+});
+
+
